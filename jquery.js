dc01d4ce8ee1cbb820f942f4b30f183d6dcd35ac
tobiasba4 <totalpsych1@gmail.com>
2016-05-04 11:21:46 +0200

    function when fail

--- jquery.js
+++ jquery.js
@@ -8,8 +8,15 @@ console.log("knappen trycktes på")
 $.get("https://api.github.com/zen", function(data, textstatus,){
 
 console.log(textstatus);
-
+//visa texten
 $('#show server text').text(data);
+
+
+
+} ).fail(function() {console.log("request var fel")})
+
+
+
 
 }
 );
